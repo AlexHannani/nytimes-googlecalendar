a228fd5daae88d9edd43b7fc0f12692da450fcf1
dpinkus87 <dylanpinkus@gmail.com>
2022-11-23 10:30:41 -0600

    added variables, psuedocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-var NYtimesAPIkey = pMMnLgkXzvtnJ9YkVw0FoZHJsXRo2vnB
+var NYtimesAPIkey = pMMnLgkXzvtnJ9YkVw0FoZHJsXRo2vnB;
+var isbn = [];
+var title = "";
+var author = "";
+var date = "";
+var NYtimeDate = "published_date";
+var NYtimeListFiction = "combined-print-and-e-book-fiction";
+var NYtimeListNonfiction ="combined-e-book-and-print-nonfiction";
+
+
+// Already have function for date from jQuery
+
+//  Fetch NYtimes list - https://api.nytimes.com/svc/books/v3/lists/{date}/{list}.json
+        // (date formate - YYYY/MM/DD)  ^(\d{4}-\d{2}-\d{2}|current)$
+        // List1 - combined-print-and-e-book-fiction
+        // List2 - combined-e-book-and-print-nonfiction
+
+//  Function to search by date - bestsellers_date
+
+
+
+//  pull ISBN from NY Times - isbn10
+
+
+
+//  store ISBN into ISBN array
+    for (var i = 0; index < isbn.length; i++) {
+        const element = array[index];
+    }
+
+//  
